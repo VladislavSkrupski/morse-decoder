b1c2263d560eec6aca40a66eb06e91fd5856e6e2
Vladislav Skrupski <vladislav.skrupski@gmail.com>
2023-01-25 11:38:27 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,44 +1,71 @@
+const {validatePlugin} = require("mocha/lib/cli/run-helpers");
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+    const SYMBOL_LENGTH = 10;
+    let convertedString = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        let temp = '';
+        const symbolString = expr.substring(i, i + SYMBOL_LENGTH);
+        if (symbolString === '*'.repeat(10)) {
+            convertedString += ' ';
+        } else {
+
+            for (let j = 0; j < symbolString.length; j += 2) {
+                const ch = symbolString.substring(j, j + 2);
+                switch (ch) {
+                    case '00':
+                        temp += '';
+                        break;
+                    case '10':
+                        temp += '.';
+                        break;
+                    case '11':
+                        temp += '-';
+                        break;
+                }
+            }
+            convertedString += MORSE_TABLE[temp];
+        }
+    }
+    return convertedString;
 }
 
 module.exports = {
